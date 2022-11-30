37cf436f9fd655426712510c1f688dc21ee1752a
Andrei <andrei_savelev@epam.com>
2022-11-30 11:55:45 +0400

    feat: Use EJS instead of others

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -6,10 +6,10 @@ const router = express.Router();
 const products: Products = [];
 const PATH = "/add-product";
 
-// We are don't call the next function, 
+// We are don't call the next function,
 // so we don't go to the next middleware
 router.get(PATH, (_, res) => {
-  res.render("add-product", { docTitle: "Add Product", path: PATH });
+  res.render("add-product", { pageTitle: "Add Product", path: PATH });
 });
 
 router.post(PATH, (req, res) => {
--- src/routes/shop.ts
+++ src/routes/shop.ts
@@ -7,7 +7,7 @@ const PATH = "/";
 
 // Root route
 router.get(PATH, (_, res) => {
-  res.render("shop", { products, docTitle: "Shop", path: PATH });
+  res.render("shop", { products, pageTitle: "Shop", path: PATH });
 });
 
 export default router;
